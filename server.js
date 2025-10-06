1c501c170ddd712bf790a43069f347e4602c9278
eduardo cl <carrioneduardo19@gmail.com>
2025-10-06 09:47:04 -0500

    Frontend terminado

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+
+//Actualización para despegar el front-end
+const cors = require('cors') //Permisos sobre el contenido a desplegar
+const path = require('path') //Express servir el frontend
+
 const productoRoutes = require('./routes/productoRoutes'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000; // puerto de la app
+
+//Actualización - Permisos cors
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,HEAD,PUT,PATCH,DELETE',
+  credentials: true
+}))
+
+//Actualización Servir los documentos HTML, CSS, JS
+app.use(express.static(path.join(__dirname, 'public')))
+
+//http://localhost:300 -> public>index.html
+app.get('/',(req, res)=> {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+})
 
 // Comunicación se realizará en JSON
 app.use(express.json());
